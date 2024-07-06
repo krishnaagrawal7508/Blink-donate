88bbda7dcea030c3163bff7e4a92f9eb10128d36
krishna agrawal <agrawalkrishna522@gmail.com>
2024-07-06 23:14:55 +0530

    added options route

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
+
 // usdc donation default
 app.get('/donate-usdc-config/', (req, res) => {
   console.log("router donate found")
